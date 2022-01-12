afc5b88e970b77a6c814b77e1f9fd16b2b4d42f7
0639Quercus <thsomsits.brie@gmail.com>
2022-01-12 22:03:06 +0700

    lab16_1.cpp completed

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,36 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
 
-	
+	int a = 5;
+	char b = 'A';
+	char &c = b;
+
+	int *x = &a;
+	char *y = &b;
+	int **z = &x;
+
+	cout << a << ' ' << b << ' ' << c << ' ';
+	cout << x << ' ' << (void*)y << ' ' << z << '\n';
+	cout << &a << ' ' <<  (void*)&b << ' ' <<  (void*)&c << ' ';
+	cout << &x << ' ' << &y << ' ' << &z << '\n';
+
+	c = 'F';
+	cout << a << ' ' << b << ' ' << c << ' ';
+	cout << x << ' ' << (void*)y << ' ' << z << '\n';
+
+	*y = 'W';
+	cout << a << ' ' << b << ' ' << c << ' ';
+	cout << x << ' ' << (void*)y << ' ' << z << '\n';
+
+	*x = 6;
+	cout << a << ' ' << b << ' ' << c << ' ';
+	cout << x << ' ' << (void*)y << ' ' << z << '\n';
+
+	**z = 7;
+	cout << a << ' ' << b << ' ' << c << ' ';
+	cout << x << ' ' << (void*)y << ' ' << z << '\n';
 	
 	return 0;
 }
